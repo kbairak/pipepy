7f4fc396e47845beef7b472711c879021bef6224
Konstantinos Bairaktaris <kbairak@transifex.com>
2021-02-16 22:56:29 +0200

    Add `jobs` function and cleanup some features that left zombie processes behind

--- src/pipepy/__init__.py
+++ src/pipepy/__init__.py
@@ -1,4 +1,4 @@
 from .exceptions import PipePyError  # noqa
 from .misc import *  # noqa
-from .pipepy import (PipePy, set_always_raise, set_always_stream,  # noqa
-                     set_interactive)
+from .pipepy import (PipePy, jobs, set_always_raise, set_always_stream,  # noqa
+                     set_interactive, wait_jobs)
